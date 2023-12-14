3da17034e65e26d8cd68b807e74cf02bdfb2d3df
harrylowkey <harydang.tech@gmail.com>
2023-12-14 23:42:28 +0700

    add some helpful helm commands

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # What I learnt
 
-
 - [CKAD](./certifications/ckad/README.md) - CKAD Certification notes
 - [HELM](./devops/helm/README.md) - Helm Charts
 - [k8s](./devops/k8s/README.md) - k8s manifests
--- devops/helm/command.sh
+++ devops/helm/command.sh
@@ -1,5 +1,20 @@
-# Build dependency
-helm dependency build .
-helm upgrade *chart-name* . --namespace *namespace* --create-namespace --install
+1. Use opensource chart
+# Add repo
+helm repo add <repo-name> <url>   # Add a repository from the internet:
+
+# Install chart from repo that installed before
+helm install <name> <chart> # Install the chart with a name
+
+# Upgrade or create new one 
+helm upgrade <release> <chart> # Upgrade a release
+helm upgrade <release-name> . --namespace <namespace> --create-namespace --install
 --create-namespace if not have namespace
 --install if not have chart otherwise upgrade
+
+
+2. Build out chart from scratch
+# Create chart direcroty with template files
+helm create <name> # Creates a chart directory along with the common files and directories used in a chart.
+
+# Build dependency
+helm dependency build .
