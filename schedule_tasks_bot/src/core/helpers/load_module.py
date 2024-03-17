5fdb0a218135b4677267d2555511cd5df73311ca
harrylowkey <harrydang.tech@gmail.com>
2024-03-17 22:22:44 +0700

    fix: load moodule

--- schedule_tasks_bot/src/core/helpers/load_module.py
+++ schedule_tasks_bot/src/core/helpers/load_module.py
@@ -7,7 +7,7 @@ def load_module(path: str):
   modules = glob.glob(path, recursive=True)
   for module in modules:
     importlib.import_module(
-      module.replace('.py', '').replace('/', '.').replace('schedule_tasks_bot.src.', ''),
+      module.replace('.py', '').replace('/', '.').replace('src.', ''),
       package=None,
     )
 
