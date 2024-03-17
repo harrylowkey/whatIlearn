a5093be4ae5245e3111785a3c6ceae23d0b52937
harrylowkey <harrydang.tech@gmail.com>
2024-03-17 22:11:21 +0700

    remove debug

--- schedule_tasks_bot/src/schedulers/send_task_scheduler.py
+++ schedule_tasks_bot/src/schedulers/send_task_scheduler.py
@@ -6,5 +6,4 @@ from schedule_tasks.task import send_tasks
 
 @app.on_event('startup')
 def send_tasks_scheduler():
-  print('ok')
-  scheduler.add_job(send_tasks, 'interval', seconds=5)
+  scheduler.add_job(send_tasks, 'cron', hour=7)
