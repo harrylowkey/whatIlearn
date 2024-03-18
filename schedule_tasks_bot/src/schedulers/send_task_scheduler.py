4a089918c7a9be8cda38c8a056ec17d4618de439
harrylowkey <harrydang.tech@gmail.com>
2024-03-18 07:47:53 +0700

    change send_task_schedule follow utc time

--- schedule_tasks_bot/src/schedulers/send_task_scheduler.py
+++ schedule_tasks_bot/src/schedulers/send_task_scheduler.py
@@ -5,4 +5,4 @@ from schedule_tasks.task import send_tasks
 
 @app.on_event('startup')
 def send_tasks_scheduler():
-  scheduler.add_job(send_tasks, 'cron', hour=7)
+  scheduler.add_job(send_tasks, 'cron', hour=1)
